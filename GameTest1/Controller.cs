842a7819ae05c392991b4c5dc21345a86990cda1
Andrey Martunyk <aniks.zoom@gmail.com>
2018-11-03 01:30:36 +0300

    Added "Controller" who take user key and return "GameActions"

---
+++ GameTest1/Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTest1
+{
+    class Controller
+    {
+        public UnitActions GetActionFromKey(ConsoleKey key)
+        {
+            UnitActions action = UnitActions.None;
+
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    action = UnitActions.MoveLeft;
+                    break;
+                case ConsoleKey.D:
+                    action = UnitActions.MoveRight;
+                    break;
+                case ConsoleKey.W:
+                    action = UnitActions.MoveTop;
+                    break;
+                case ConsoleKey.S:
+                    action = UnitActions.MoveDown;
+                    break;
+                default:
+                    //никак не реагируем при нажатиии незадекларируемой клавиши
+                    break;
+            }
+
+            return action;
+        }
+    }
+}
---
+++ GameTest1/GameActions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameTest1
+{
+    public enum UnitActions : byte
+    {
+        None,
+        MoveRight,
+        MoveLeft,
+        MoveDown,
+        MoveTop,
+        Stop,
+        Shoot,
+        Die,
+        GetDamage,
+        Attack
+    }
+}
